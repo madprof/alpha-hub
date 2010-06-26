e2a140c4df6c8daa171a002625b5920da79079f7
|ALPHA| Mad Professor <alpha.mad.professor@gmail.com>
2010-06-26 09:17:32 -0400

    Custom string types and better docs.

--- hub/model.py
+++ hub/model.py
@@ -4,19 +4,35 @@
 """
 model.py - data model
 
-- model classes perform important conversion on data handed to
-  them in the constructor; see "password" for User for example
-- all times are UTC in the database; convert at UI if necessary
+- some model classes perform important conversions on data
+  handed to them; see User.password for example
+
+- some model classes perform important sanity checks on data
+  handed to them; TODO: need an example
+
+- all times are in UTC in the model; convert in the user
+  interface if necessary
+
+- string lengths are somewhat contentious between database
+  systems; for example MySQL apparently doesn't like variable
+  length strings quite as much as SQLite; so we use fixed
+  length strings all over, but we give them custom names,
+  for example we define Password = String(128) because we
+  use SHA-512 for passwords and need that length string to
+  store them; TODO: does this really work?
+
+- related to string lengths, how much space do we need to
+  store network addresses? as long as we stay "numeric"
+  the worst case is clear: an IPv6 address needs up to 39
+  characters, e.g. "2001:0f68:0000:0000:0000:0000:1986:69af",
+  if we add a port we need 6 more, e.g. ":27961", if we add
+  a CIDR we need 4 more, e.g. "/101"; for a total of 49 or
+  so characters; domain names are a bit more contentious
+  again since very few people seem sure about the exact
+  limits; we use Address = String(256) for now and we'll
+  see if we ever need more
+
 - we never delete anything; at most we mark things inactive
-- an IPv6 address is up to 39 characters long in the usual char
-  format, e.g. "2001:0f68:0000:0000:0000:0000:1986:69af"; if
-  we add a port, we need six more characters, e.g. ":27961";
-  if we add a CIDR, we need four more chars, e.g. "/101"; so
-  we need a total of 49 chars in the worst case; round up and
-  you get 64 :-) domain names are a whole different can of
-  worms, but I think 255 for FQDN + 6 for port would be enough;
-  probably easiest to simply store either as an arbitrary length
-  string, just in case...
 """
 
 from datetime import datetime
@@ -27,21 +43,37 @@ from sqlalchemy import Boolean, Integer, String, Text, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+GUID = Tiny = String(32)
+Short = String(64)
+Password = Medium = String(128)
+Address = Long = String(256)
+
 Base = declarative_base()
 
 class Player(Base):
     """
     Player observed on a game server.
+
+    - more information is available but recording the minimum should be
+      enough for now
+
+    - the "bad names" feature can be implemented without keeping score
+      of how often someone was warned, but maybe we should add a field
+      that can be used "open-ended" by other modules? actually, for bad
+      names it doesn't matter since we record the bad name anyway, no
+      way around it unless we filter them out *before* they hit the
+      userinfo change - but then we'd have no record of them...
     """
     __tablename__ = 'players'
 
     id = Column(Integer, Sequence('players_ids'), nullable=False, unique=True)
-    name = Column(String(64), primary_key=True)
-    address = Column(Text, primary_key=True)
-    guid = Column(String(64), primary_key=True)
-    server = Column(String(64), primary_key=True)
-    first = Column(DateTime, nullable=False)
-    last = Column(DateTime, nullable=False)
+    name = Column(Tiny, primary_key=True, nullable=False,
+                  doc="ioq3 player name 20 chars")
+    address = Column(Address, primary_key=True, doc="ip address")
+    guid = Column(GUID, primary_key=True, doc="ioq3 GUID 32 chars")
+    server = Column(Address, primary_key=True, doc="ip address")
+    first = Column(DateTime, nullable=False, doc="on insert")
+    last = Column(DateTime, nullable=False, doc="on insert and update")
 
     def __init__(self, name, address, guid, server):
         self.name = name
@@ -59,30 +91,33 @@ class User(Base):
     """
     User registered with the hub.
 
-    TODO: activating an account to verify email versus disabling an account
     TODO: only admins? only members? everybody?
+    TODO: permission mechanism missing
     """
     __tablename__ = 'users'
 
     id = Column(Integer, Sequence('users_ids'), nullable=False, unique=True)
-    login = Column(String(64), primary_key=True)
-    password = Column(String(128), nullable=False)
-    name = Column(String(64), nullable=False)
-    email = Column(String(128), nullable=False, unique=True)
-    active = Column(Boolean, nullable=False)
+    login = Column(Tiny, primary_key=True)
+    password = Column(Password, nullable=False, doc="sha512 hash")
+    name = Column(Short, nullable=False, doc="full name")
+    email = Column(Address, nullable=False, unique=True)
+    activated = Column(Boolean, nullable=False, doc="activated, email valid")
+    disabled = Column(Boolean, nullable=False, doc="disabled, can't login")
     first = Column(DateTime, nullable=True)
     last = Column(DateTime, nullable=True)
 
-    def __init__(self, login, password, name, email, active=False):
+    def __init__(self, login, password, name, email, activated=False,
+                 disabled=False):
         self.login = login
         self.password = sha512(password).hexdigest()
         self.name = name
         self.email = email
-        self.active = active
+        self.activated = activated
+        self.disabled = disabled
 
     def __repr__(self):
-        return "User<login: %s; name: %s; email: %s; active: %s>" % (
-            self.login, self.name, self.email, self.active
+        return "User<login: %s; name: %s; email: %s; activated: %s>" % (
+            self.login, self.name, self.email, self.activated
         )
 
 class GameAdmin(Base):
@@ -105,9 +140,9 @@ class GameAdmin(Base):
     __tablename__ = 'game_admins'
 
     id = Column(Integer, Sequence('game_admins_ids'), primary_key=True)
-    address = Column(Text, nullable=False)
-    guid = Column(String(64), nullable=False)
-    password = Column(String(128), nullable=False)
+    address = Column(Address, nullable=False, doc="ip address")
+    guid = Column(GUID, primary_key=True, doc="ioq3 GUID 32 chars")
+    password = Column(Password, nullable=False, doc="sha512 hash")
     active = Column(Boolean, nullable=False)
 
     user_id = Column(Integer, ForeignKey('users.id'))
@@ -140,9 +175,9 @@ class Server(Base):
     __tablename__ = 'servers'
 
     id = Column(Integer, Sequence('servers_ids'), nullable=False, unique=True)
-    guid = Column(String(64), primary_key=True)
-    address = Column(Text, primary_key=True)
-    rcon = Column(String(64), nullable=False)
+    guid = Column(GUID, primary_key=True)
+    address = Column(Address, primary_key=True)
+    rcon = Column(Short, nullable=False)
     active = Column(Boolean, nullable=False)
     first = Column(DateTime, nullable=True)
     last = Column(DateTime, nullable=True)
@@ -161,6 +196,9 @@ class Server(Base):
 if __name__ == "__main__":
     p = Player("|ALPHA| CCCP", "1.2.3.4", "CCCPCCCPCCCPCCCPCCCPCCCPCCCPCCCP",
                "3.4.5.6:27964")
+    print p
+    p2 = Player("a", "b", "c", "d")
+    print p2
     print p
 
     u = User("mad", "gagagagaga", "|ALPHA| Mad Professor",
