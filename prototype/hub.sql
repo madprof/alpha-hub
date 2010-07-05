6e5811fca0e5b161c4ed8746b37b1146ee153599
|ALPHA| Mad Professor <alpha.mad.professor@gmail.com>
2010-07-05 12:52:51 -0400

    Added table for gossip.

--- prototype/hub.sql
+++ prototype/hub.sql
@@ -1,6 +1,7 @@
--- creates sqlite database for hub.py prototype
+-- create sqlite database for |ALPHA| Hub prototype
 
-CREATE TABLE Players (
+-- actual players from game servers we run and trust
+CREATE TABLE IF NOT EXISTS Players (
     ip VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     guid VARCHAR NOT NULL,
@@ -11,19 +12,45 @@ CREATE TABLE Players (
     PRIMARY KEY (ip, name, guid, server, port)
 );
 
-CREATE TRIGGER insertPlayer AFTER INSERT ON Players
+CREATE TRIGGER IF NOT EXISTS insertPlayer AFTER INSERT ON Players
 BEGIN
   UPDATE Players
-    SET first = datetime("now")
+    SET first = datetime("now"), last = datetime("now")
       WHERE rowid = new.rowid;
+END;
+
+CREATE TRIGGER IF NOT EXISTS updatePlayer AFTER UPDATE ON Players
+BEGIN
   UPDATE Players
     SET last = datetime("now")
       WHERE rowid = new.rowid;
 END;
 
-CREATE TRIGGER updatePlayer AFTER UPDATE ON Players
+-- gossip from other hubs we keep only for reference
+CREATE TABLE IF NOT EXISTS Gossips (
+    ip VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    guid VARCHAR NOT NULL,
+    server VARCHAR NOT NULL,
+    port VARCHAR NOT NULL,
+    origin VARCHAR NOT NULL,
+    count INTEGER DEFAULT NULL,
+    first TIMESTAMP DEFAULT NULL,
+    last TIMESTAMP DEFAULT NULL,
+    PRIMARY KEY (ip, name, guid, server, port, origin)
+);
+
+CREATE TRIGGER IF NOT EXISTS insertGossip AFTER INSERT ON Gossips
 BEGIN
-  UPDATE Players
-    SET last = datetime("now")
+  UPDATE Gossips
+    SET first = datetime("now"), last = datetime("now"), count = 0
+      WHERE rowid = new.rowid;
+END;
+
+CREATE TRIGGER IF NOT EXISTS updateGossip AFTER UPDATE ON Gossips
+BEGIN
+  UPDATE Gossips
+    SET last = datetime("now"),
+        count = (SELECT count FROM Gossips WHERE rowid = new.rowid)+1
       WHERE rowid = new.rowid;
 END;
