45259ae481529f1cdf7f786d0231f4a41233030a
|ALPHA| Mad Professor <alpha.mad.professor@gmail.com>
2010-07-05 00:14:28 +0000

    Script to create sqlite database.

---
+++ hub/prototype/hub.sql
@@ -0,0 +1,27 @@
+-- creates sqlite database for hub.py prototype
+
+CREATE TABLE Players (
+    ip VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    guid VARCHAR NOT NULL,
+    first TIMESTAMP,
+    last TIMESTAMP,
+    PRIMARY KEY (ip, name, guid)
+);
+
+CREATE TRIGGER insertPlayer AFTER INSERT ON Players
+BEGIN
+  UPDATE Players
+    SET first = datetime("now")
+      WHERE rowid = new.rowid;
+  UPDATE Players
+    SET last = datetime("now")
+      WHERE rowid = new.rowid;
+END;
+
+CREATE TRIGGER updatePlayer AFTER UPDATE ON Players
+BEGIN
+  UPDATE Players
+    SET last = datetime("now")
+      WHERE rowid = new.rowid;
+END;
